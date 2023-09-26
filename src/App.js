dc05a2477b31b7fdb3bdf085985f715d8911a471
AD1121 <sinagh30@gmail.com>
2023-09-26 19:10:57 +0200

    Pages files added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css';
-import BlogList from './components/BlogList'
-import BlogPost from './components/BlogPost';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import BlogList from "./components/BlogList";
+import BlogPost from "./components/BlogPost";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <h1>Welcome</h1>
       </header>
       <main>
         <Router>
           <Routes>
-            <Route path='/' Component={BlogList} />
-            <Route path='/:id' Component={BlogPost} />
+            <Route path="/" Component={BlogList} />
+            <Route path="/:id" Component={BlogPost} />
           </Routes>
         </Router>
       </main>
     </div>
-  )
+  );
 }
 
 export default App;
