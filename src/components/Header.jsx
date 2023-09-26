261df89b1a6e36f6c886a1dda30d22e836cec5ce
AD1121 <sinagh30@gmail.com>
2023-09-26 19:33:19 +0200

    #41 Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <ul>
+        <li>Shine & Clean</li>
+        <li className="push-right">
+          {" "}
+          <Link className="links" to="/">
+            Home
+          </Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link className="links" to="/about">
+            About{" "}
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link className="links" to="/contact">
+            Contact
+          </Link>{" "}
+        </li>
+        <li className="border">
+          {" "}
+          <Link className="links" to="/login">
+            {" "}
+            Login <i className="fa-solid fa-user"></i>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
