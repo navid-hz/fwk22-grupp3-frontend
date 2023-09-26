0bc969b8cb757d8300045f51fcfe2914c8a543c3
AD1121 <sinagh30@gmail.com>
2023-09-26 19:58:12 +0200

    #41 CSS Style and buttons added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,19 @@
 import { Link } from "react-router-dom";
+import "./header.css";
 
 const Header = () => {
   return (
     <div>
       <ul>
-        <li>BLOG</li>
+        <li className="links">BLOG</li>
         <li className="push-right">
-          {" "}
           <Link className="links" to="/">
             Home
-          </Link>{" "}
-        </li>
-        <li>
-          {" "}
-          <Link className="links" to="/about">
-            About{" "}
           </Link>
         </li>
         <li>
-          {" "}
-          <Link className="links" to="/contact">
-            Contact
-          </Link>{" "}
-        </li>
-        <li className="border">
-          {" "}
-          <Link className="links" to="/login">
-            {" "}
-            Login <i className="fa-solid fa-user"></i>
+          <Link className="links" to="/create-blog">
+            Create Blog
           </Link>
         </li>
       </ul>
