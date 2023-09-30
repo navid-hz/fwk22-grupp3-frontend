578015fcf2fd9ee3ab5558945a7d2b4cad7003d6
AD1121 <sinagh30@gmail.com>
2023-09-30 23:47:48 +0200

    #56 GET blog by id almost done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import 'react-multi-carousel/lib/styles.css'
+import "react-multi-carousel/lib/styles.css";
 import Home from "./components/Home";
 import CreateBlog from "./Pages/CreateBlog";
 import Blog from "./Pages/Blog";
--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Blog = () => {
+  const [blog, setBlog] = useState();
+
+  // Get the blogId
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchUserBlogs = async () => {
+      try {
+        const response = await fetch(`https://dummyjson.com/posts/${id}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await response.json();
+
+        setBlog(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUserBlogs();
+  }, []);
+
+  console.log(blog.title);
+
   return (
-    <div>
-      <h1>Blog Page</h1>
+    <div className="container-blog">
+      <p>May 20 - Written By USer</p>
+      {/* <h1>{blog.title}</h1>
+      <p>{blog.body}</p> */}
     </div>
   );
 };
