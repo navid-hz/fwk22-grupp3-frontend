8ab7b5d775d92a530b91b36da5f71331ac1a0d57
AD1121 <sinagh30@gmail.com>
2023-10-02 21:20:31 +0200

    #56 Blog Page Fetching done

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -2,38 +2,60 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Blog = () => {
-  const [blog, setBlog] = useState();
+  const [blogs, setBlogs] = useState([]);
 
   // Get the blogId
   const { id } = useParams();
 
+  console.log(id);
+
   useEffect(() => {
-    const fetchUserBlogs = async () => {
-      try {
-        const response = await fetch(`http://localhost:5000/blogs/${id}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const data = await response.json();
-
-        setBlog(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchUserBlogs();
+    fetchAllBlogs();
   }, []);
 
-  console.log(blog);
+  const fetchAllBlogs = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/blogs/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+
+      setBlogs(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  function formatTimestamp(timestamp) {
+    const dateObject = new Date(timestamp);
+
+    const year = dateObject.getFullYear();
+    const month = dateObject.getMonth() + 1; // Adding 1 because months are zero-indexed
+    const day = dateObject.getDate();
+
+    const hours = dateObject.getHours();
+    const minutes = dateObject.getMinutes();
+    const seconds = dateObject.getSeconds();
+
+    // Pad single-digit values with leading zeros
+    const pad = (value) => (value < 10 ? `0${value}` : value);
+
+    return `${year}-${pad(month)}-${pad(day)} ${pad(hours)}:${pad(
+      minutes
+    )}:${pad(seconds)}`;
+  }
+
+  const timestamp = blogs.createdAt;
+  const formattedDate = formatTimestamp(timestamp);
 
   return (
     <div className="container-blog">
-      <p>May 20 - Written By USer</p>
-      <h1>{blog.title}</h1>
-      <p>{blog.content}</p>
+      <p className="date">{formattedDate}</p>
+      <h1 className="title-blog-id">{blogs.title}</h1>
+      <p>{blogs.content}</p>
     </div>
   );
 };
