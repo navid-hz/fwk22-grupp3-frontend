21f08b23fcb814cdce6cca61e712a0d05c70b0dc
Linn Vailt <113286790+linn-vailt@users.noreply.github.com>
2023-10-03 10:46:02 +0200

    firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCdLTkboLIFZyNKlqT0eREpgcnwyVp-zGg",
-    authDomain: "fir-server-g3.firebaseapp.com",
-    projectId: "fir-server-g3",
-    storageBucket: "fir-server-g3.appspot.com",
-    messagingSenderId: "433266651358",
-    appId: "1:433266651358:web:035298cba361b4d194124f"
+
   }
 
 export default firebaseConfig
