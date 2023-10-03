ecc858173689070a86052c21742cb68fa71a1f82
Linn Vailt <113286790+linn-vailt@users.noreply.github.com>
2023-10-03 10:45:26 +0200

    firebase

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,36 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
-import * as firebase from 'firebase/app'
-import 'firebase/firestore'
 
 function BlogList() {
   const [post, setPosts] = useState([]);
-  const db = firebase/firestore()
+  const backendURL = 'http://localhost:5000/blogs';
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = [];
-        const querySnapshot = await db.collection('blogposts').get();
-        querySnapshot.forEach((doc) => {
-          data.push({ ...doc.data(), id: doc.id });
-        });
+    fetch(backendURL)
+      .then((response) => response.json())
+      .then((data) => {
         setPosts(data);
-      } catch (error) {
+      })
+      .catch((error) => {
         console.error('Error fetching blog posts:', error);
-      }
-    };
-
-    fetchData();
-  }, [db]);
+      });
+  }, []);
 
   return (
     <div>
       <h2>Blog Posts</h2>
       <ul>
-        {posts.map((blog) => (
-          <li key={blog.id}>
-            <Link to={`/blog/${blog.id}`}>
+        {post.map((blog) => (
+          <li key={blog._id}>
+            <Link to={`/blog/${blog._id}`}>
               <h3>{blog.title}</h3>
             </Link>
             <p>{blog.content}</p>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-import firebaseConfig from './firebaseConfig';
-
-firebase.initializeApp(firebaseConfig);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
